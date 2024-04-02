3be6042d016092362b6232278633ae8dd0cb08b8
EricZapater <ezapaterm@gmail.com>
2024-04-02 10:24:35 +0200

    test nginx.conf v2

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,9 +4,10 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL as string;
 
 const api = axios.create({
   baseURL: baseUrl,
-  withCredentials: true,
+  //withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    "Acces-Control-Allow-Credentials": true,
   },
 });
 
